c97ee16184c46b728ef6ee21797639576b79b62b
Jose Rico <jmrico01@gmail.com>
2022-08-29 22:46:27 -0400

    755 with edits

--- scripts/setup_https_1.sh
+++ scripts/setup_https_1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 sudo apt update
 sudo apt -y install snapd
 echo "Snap updated. Re-log and run setup_https_2.sh to continue."
--- scripts/setup_https_2.sh
+++ scripts/setup_https_2.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 sudo snap install core
 sudo snap refresh core
 sudo snap install --classic certbot
