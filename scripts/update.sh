50b6f1d475e7f28a2ee141e1fc4ff0bcee88df63
Jose Rico <jmrico01@gmail.com>
2023-05-20 23:25:15 -0400

    upd update.sh and systemd

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
-rm -rf yorstory*
+mv yorstory yorstory-prev
+mv yorstory.tar.gz yorstory-prev.tar.gz
+
 curl $1 --output yorstory.tar.gz
 tar -xf yorstory.tar.gz
+cp -r yorstory-prev/data yorstory/data
+
 sudo systemctl restart yorstory
 sudo systemctl status yorstory
