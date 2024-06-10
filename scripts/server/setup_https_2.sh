d9af100f8bba1c93ebdf0a76fc0e5513a40ac978
Jose Rico <jmrico01@gmail.com>
2024-06-09 23:34:37 -0400

    rm

--- scripts/server/setup_https_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-sudo snap install core
-sudo snap refresh core
-sudo snap install --classic certbot
-sudo ln -sf /snap/bin/certbot /usr/bin/certbot
-sudo certbot certonly --standalone --agree-tos --email kapricornmedia@gmail.com -d yorstory.ca -d www.yorstory.ca
